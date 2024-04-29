fd6a7eb2d13eb60abb0671432447ae2d3873ca4f
Alba <allbaluccia@gmail.com>
2024-04-29 07:52:53 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import "bootstrap";
+//import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Hello from the console!");
+
+  let quien = ["el Perro", "el gato", "la lluvia"];
+  let accion = ["se comió", "orino", "mojo"];
+  let aCualCosa = ["la tarea", "la casa", "el carro"];
+  let cuando = [
+    "ayer",
+    "cuando dormia",
+    "durante el almuerzo",
+    "a la hora del ejercicio"
+  ];
+
+  let generateExcuse = () => {
+    let whoQuien = [Math.floor(Math.random() * quien.length)];
+    let actAccion = [Math.floor(Math.random() * accion.length)];
+    let acualWhat = [Math.floor(Math.random() * aCualCosa.length)];
+    let cuandoWhen = [Math.floor(Math.random() * cuando.length)];
+
+    return `${quien[whoQuien]} ${accion[actAccion]} ${aCualCosa[acualWhat]} ${cuando[cuandoWhen]}`;
+  };
+
+  document.querySelector("#excuse").textContent = generateExcuse();
 };
